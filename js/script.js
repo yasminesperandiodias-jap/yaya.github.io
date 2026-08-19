262ebf1a8a95847f28a3878e0d38980e3964d6c1
Yaasjaponesa <yasmin.esperandio.dias@escola.pr.gov.br>
2026-08-19 08:01:33 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,42 @@
+// =============================
+// MODO ESCURO
+// =============================
 
+const botaoTema =
+    document.getElementById("botaoTema");
+
+function alternarTema() {
+
+    document.body.classList.toggle("escuro");
+
+    if (document.body.classList.contains("escuro")) {
+
+        botaoTema.textContent = "☀ Modo claro";
+
+    } else {
+
+        botaoTema.textContent = "🌙 Modo escuro";
+
+    }
+}
+
+// =============================
+// ALTERAR TEXTO
+// =============================
+
+const botaoMensagem =
+    document.getElementById("botaoMensagem");
+
+const mensagem =
+    document.getElementById("mensagem");
+
+function alterarTexto() {
+
+    mensagem.textContent =
+        "O texto foi alterado pelo JavaScript!";
+
+}
+
+botaoMensagem.addEventListener("click", alterarTexto);
+
+botaoTema.addEventListener("click", alternarTema);
